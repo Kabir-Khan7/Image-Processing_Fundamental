4b46fc793f8c349fa8602398fa0ada1505e342fd
Kabir Khan <kabirkhurshid.official@gmail.com>
2025-04-07 00:16:37 +0500

    Create crop_image.py

---
+++ crop_image.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+
+# This gets the folder where your Python file is
+folder = os.path.dirname(os.path.abspath(__file__))
+
+# This creates the path to your image
+image_path = os.path.join(folder, 'image_py.jpg')
+
+# Load the image
+img = cv2.imread(image_path)
+print(img.shape)
+
+#Cropped image
+cropped_img = img[220:400, 320:600]
+
+cv2.imshow('img', img)
+cv2.imshow('cropped_img', cropped_img)
+cv2.waitKey(0)
